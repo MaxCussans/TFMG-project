52f2a31e5792687f61a37ac13902251fa0f3d5d1
a <a>
2017-03-15 15:31:46 +0000

    intial commit

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator)target;
+
+        //if any changes made
+        if (DrawDefaultInspector())
+        {
+            //if auto update is true (ticked in inspector)
+            if (mapGen.autoUpdate)
+            {
+                //update the map
+                mapGen.GenerateMap();
+            }
+        }
+
+        if(GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+	
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int MapWidth, int MapHeight, float scale)
+    {
+        float[,] NoiseMap = new float[MapWidth, MapHeight];
+
+        if(scale <= 0)
+        {
+            scale = 0.00001f;
+        }
+
+        for (int y =0; y < MapHeight; y++)
+        {
+            for (int x = 0; x < MapWidth; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                NoiseMap[x,y] = perlinValue;
+            }
+        }
+        return NoiseMap;
+    }
+}
